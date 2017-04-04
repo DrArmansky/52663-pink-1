afb517309f005e5a2d3be1d29ec4821185b47d96
Kirill Sagan <sagan.k89@gmail.com>
2017-04-04 17:54:02 +0300

    pp for index and corrects

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task("style", function() {
         "last 2 Edge versions"
       ]}),
       mqpacker({
-          sort: true
+          sort: false
       })
     ]))
     .pipe(gulp.dest("build/css"))
